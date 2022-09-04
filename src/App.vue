2fb57e1d5986bea713525cff4b4e1a790d4767e2
Cha Haneum <developer_haneum_11@icloud.com>
2022-09-04 17:55:26 +0900

    ...

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,13 @@
 import { ref } from "vue";
 
 const value = ref(0);
-let v = 0;
 
 const increase = () => {
   value.value++;
-};
-const i = () => {
-  v++; // 렌더링 되지 않음
-  console.log(v);
 };
 </script>
 
 <template>
   <div>{{ value }}</div>
   <button @click="increase">increase</button>
-  <div>{{ v }}</div>
-  <button @click="i">increase</button>
 </template>
